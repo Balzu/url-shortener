53925e2bd792ae69dbe011266b7499d216dfb3e8
Balzu <francescobalzano10@gmail.com>
2017-04-06 12:00:06 +0200

    Refactored in 2 subprojects

--- src/main/java/it/unipi/mcsn/pad/core/message/MessageStatus.java
+++
@@ -1,8 +0,0 @@
-package it.unipi.mcsn.pad.core.message;
-
-public enum MessageStatus {
-	
-	SUCCESS, ERROR, UNKNOWN,      //Please check if you need them
-	NOT_FOUND
-
-}
--- src/main/java/it/unipi/mcsn/pad/core/message/MessageType.java
+++
@@ -1,6 +0,0 @@
-package it.unipi.mcsn.pad.core.message;
-
-public enum MessageType {
-	PUT,GET, REMOVE,
-	PUSH,PULL,REPLY       //Checks these last 3 if really needed
-}
